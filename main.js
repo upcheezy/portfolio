c85d6525bb195cb7b593b8855ad764b9e38088e5
Matthew Upchurch <upcheezy@gmail.com>
2020-02-10 08:54:47 -0500

    updated social media

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
-// map options
-    var options = {
-      center: [33.83,-80.87333],
-      zoom: 8,
-      zoomControl: false
-    }
+function burgerClick() {
+  $('.fa-bars').on('click', function (event) {
+    alert('hamburger click')
+  });
+}
 
-    // create Leaflet map
-    var map = L.map('map', options);
-
-    // request some slippy map tiles
-    var tiles = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      subdomains: 'abcd',
-      minZoom: 1,
-      maxZoom: 16,
-      ext: 'png'
-    });
-
-    // add tiles to the map
-    map.addLayer(tiles);
+$(burgerClick())
