79b3cc220a39adb99a62104a8c581d93854a3982
Matthew Upchurch <upcheezy@gmail.com>
2020-02-11 10:07:46 -0500

    added new files and everything working except jquery

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 function burgerClick() {
-  $('.fa-bars').on('click', function (event) {
-    alert('hamburger click')
+  $('.nav-bar').on('click', '.fa-bars', function(event) {
+    // alert('hamburger click')
+    // $('.fa-bars').hide()
+    $('this').html(`
+          <div class='nav-menu-popout'>
+            <ul>
+                <li><a href="#about-me">About me</a></li>
+                <li><a href="#project-container">Projects</a></li>
+            </ul>
+        </div>`
+    )
   });
 }
 
