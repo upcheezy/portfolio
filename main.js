f7be78b6bba5f778a2f17ffaee61e78c8abaeae3
Matthew Upchurch <upcheezy@gmail.com>
2020-02-11 11:52:17 -0500

    finalized portfolio

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 function burgerClick() {
-  $('.nav-bar').on('click', '.fa-bars', function(event) {
+  $('.nav-bar').on('click', function(event) {
     // alert('hamburger click')
     // $('.fa-bars').hide()
-    $(this).parent().append(`
-          <div class='nav-menu-popout'>
-            <ul>
-                <li><a href="#about-me">About me</a></li>
-                <li><a href="#project-container">Projects</a></li>
-            </ul>
-        </div>`
-    )
-    console.log($(this).parent().attr('class'))
+    $('.nav-menu').toggleClass('nav-menu-popout');
   });
 }
 
